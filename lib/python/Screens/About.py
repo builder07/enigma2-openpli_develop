bc8c3595d98fd502f95c02a141d51ab18b6ccb0f
builder07 <47144637+builder07@users.noreply.github.com>
2020-07-02 15:25:28 +0300

    Update About.py

--- lib/python/Screens/About.py
+++ lib/python/Screens/About.py
@@ -105,8 +105,7 @@ class About(Screen):
 		AboutText += hddinfo + "\n\n" + _("Network Info:")
 		for x in about.GetIPsFromNetworkInterfaces():
 			AboutText += "\n" + x[0] + ": " + x[1]
-		uptime = about.getBoxUptime().split(':')
-		AboutText += '\n\n' + uptime[0] + ": " + uptime[1]
+		AboutText += '\n\n' + _("Uptime") + ": " + about.getBoxUptime()
 			
 		AboutText += "\n"
 		AboutText += "\n"
